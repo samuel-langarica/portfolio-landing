40a6948ed999105db44bc7f162e59ad4b3279c2d
Samuel Langarica <samuel@Samuels-MacBook-Air.local>
2025-07-15 12:44:54 -0600

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>♞</text></svg>" />
+      </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
       </body>
